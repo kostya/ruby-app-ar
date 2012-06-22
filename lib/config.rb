7ba6ffaa46c85edc9c078a36907f49f9fc778ae1
Konstantin Makarchev <kostya27@gmail.com>
2012-06-23 02:13:36 +0400

    # format

--- lib/config.rb
+++ lib/config.rb
@@ -6,7 +6,11 @@ App.config.define :default_ar do
   database_yml do
     @database_yml ||= begin
       require 'erb'
-      YAML.load(ERB.new(File.read(File.join(App.root, %w{config database.yml} ))).result)
+      YAML.load(
+        ERB.new(
+          File.read(File.join(App.root, %w{config database.yml} ))
+        ).result        
+      )
     rescue
       raise "Not found config/database.yml"
     end
