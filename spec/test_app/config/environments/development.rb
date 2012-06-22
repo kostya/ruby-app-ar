29339020dd0be6d07b29bb3fa3f741a325271369
Konstantin Makarchev <kostya27@gmail.com>
2012-06-23 02:00:51 +0400

    # remove usless files

--- spec/test_app/config/environments/development.rb
+++
@@ -1,8 +0,0 @@
-# Configs for development env
-
-App.config.define :development do
-
-  # Set log level for App.logger
-  log_level Logger::DEBUG
-
-end
--- spec/test_app/config/environments/production.rb
+++
@@ -1,8 +0,0 @@
-# Configs for production env
-
-App.config.define :production do
-
-  # Set log level for App.logger
-  log_level Logger::WARN
-
-end
